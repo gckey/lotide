e614fafc986d3f30345530ea837df375535967f0
Abdurahman <abdorehman@gmail.com>
2023-05-22 21:15:34 -0600

    Implement flatten

---
+++ flatten.js
@@ -0,0 +1,58 @@
+// Function to compare two arrays and determine if they are a perfect match.
+const eqArrays = function(array1, array2) {
+  // Check if the arrays have different lengths
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  // Iterate over the elements of the arrays
+  for (let i = 0; i < array1.length; i++) {
+    // Check if the current element is an array itself
+    if (Array.isArray(array1[i]) && (eqArrays(array1[i], array2[i]) === false)) {
+      // If the current element is an array and the recursive call to eqArrays returns false, it means the nested arrays are not a perfect match
+      return false;
+    }
+    // Check if the current element is not an array and if it's not equal in both arrays
+
+    if (!(Array.isArray(array1[i])) && array1[i] !== array2[i]) {
+      // if the element is not an array and the items are not equal then return false
+      return false;
+    }
+  }
+  return true;
+};
+
+// FUNCTION IMPLEMENTATION FOR assertArraysEqual
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual,expected)) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const flatten = function(arr) {
+  /* Initialize an empty array that will be returned with all the elements.*/
+  const flattenedArray = [];
+  // iterate through each element in the provided array
+  for (let i = 0; i < arr.length; i++) {
+    //Check whether the current element is an array using Array.isArray()
+    if (Array.isArray(arr[i])) {
+      //iterate through each element of the nested array
+      for (let j = 0; j < arr[i].length; j++) {
+        flattenedArray.push(arr[i][j]);
+      }
+    } else {
+      flattenedArray.push(arr[i]);
+    }
+  }
+  return flattenedArray;
+};
+
+// Tests
+let y = [1, 2, [3, 4], 5, [6]];
+flatten(y);
+assertArraysEqual(y, [1, 2, 3, 4, 5, 6]);
+
+let z = [[10, 20], [30, 40, 50], [60]];
+flatten(z);
+assertArraysEqual(z, [10, 20, 30, 40, 50, 60]);
