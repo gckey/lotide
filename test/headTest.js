8f7b525c4e056b39392ece072bdfbbe070725bba
Abdurahman <abdorehman@gmail.com>
2023-06-09 22:40:05 -0600

    Implement headTest with Mocha and Chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,11 +1,27 @@
-// test/headTest.js
+// Incorporate Mocha and Chai for test/headTest.js
 
-const assertEqual = require('../assertEqual');
-const head = require('../head');
+const assert = require('chai').assert;
+const head   = require('../head');
 
-/* TEST CODE */
-assertEqual(head([]), undefined);
-assertEqual(head([1]), 1);
-assertEqual(head([23, 'hello', 13]), 23);
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+describe("#head", () => {
+  it("returns undefined for []", () => {
+    assert.strictEqual(head([]), undefined);
+  });
+
+  it("returns '1' for ['1']", () => {
+    assert.strictEqual(head(['1']), '1');
+  });
+
+  it("returns 23 for [23, 'hello', 13]", () => {
+    assert.strictEqual(head([23, 'hello', 13]), 23);
+  });
+
+  it("returns 5 for [5, 6, 7]", () => {
+    assert.strictEqual(head([5, 6, 7]), 5);
+  });
+
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+  
+});
