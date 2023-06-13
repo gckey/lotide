5e1779fb5351f6e60d42181b1f37bd012ea52676
Abdurahman <abdorehman@gmail.com>
2023-06-12 22:49:47 -0600

    Refactor 'assertEqual' function

--- assertEqual.js
+++ assertEqual.js
@@ -1,4 +1,4 @@
-// FUNCTION IMPLEMENTATION assertEqual.js
+/* FUNCTION IMPLEMENTATION assertEqual.js */
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
