9d8ed205504116f2901c2d261755df426da61049
Abdurahman <abdorehman@gmail.com>
2023-06-13 17:42:06 -0600

    Refractor head.js function

--- head.js
+++ head.js
@@ -1,8 +1,7 @@
 // head.js
-const assertEqual = require('./assertEqual');
 
 /* a function which returns the first item in the array. */
-let newArr;
+// let newArr;
 const head = function(array) {
   if (array.length === 0) {
     return undefined;
