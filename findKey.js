d39f3e21295e51840fa1aab40b1cae25b501326b
Abdurahman <abdorehman@gmail.com>
2023-06-14 22:20:44 -0600

    Move test code, refractor to module

--- findKey.js
+++ findKey.js
@@ -1,31 +1,12 @@
-const assertEqual = require('./assertEqual');
 
-/*
-Function that scans an object and returns the first key for which the callback returns a truthy value 
- */
 const findKey = function(object, callback) {
-  // Get a list of all the keys in an object
   const keys = Object.keys(object);
-  // Loop through each keys of the object
   for (const key of keys) {
-    // Call the callback function on the value associated with the current key
     if (callback(object[key])) {
-      // If the callback returns a truthy value, return the current key
       return key;
     }
   }
-  //if no key with the given value is found, return `undefined`
   return undefined;
 };
 
-//TEST CODE
-const output1 = findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}, x => x.stars === 2); // => "noma"
-// console.log(output1);
-assertEqual(output1, "noma");
+module.exports = findKey;
