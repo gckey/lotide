0680159db9fcd35c2aa569d09c838543f6301e73
Abdurahman <abdorehman@gmail.com>
2023-06-14 23:03:29 -0600

    Add export module, move test code

--- takeUntil.js
+++ takeUntil.js
@@ -1,6 +1,3 @@
-const assertArraysEqual = require('./assertArraysEqual');
-
-/*Function that keep collecting items from a provided array until the callback provided returns a truthy value */
 const takeUntil = function(array, callback) {
   //Create an empty array to store the result
   const results = [];
@@ -18,15 +15,4 @@ const takeUntil = function(array, callback) {
   return results;
 };
 
-//TEST CODE
-const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-const results1 = takeUntil(data1, x => x < 0);
-// console.log(results1);
-assertArraysEqual((results1), [ 1, 2, 5, 7, 2 ]);
-
-console.log('---');
-
-const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-const results2 = takeUntil(data2, x => x === ',');
-// console.log(results2);
-assertArraysEqual((results2), [ "I've", 'been', 'to', 'Hollywood' ]);
+module.exports = takeUntil;
